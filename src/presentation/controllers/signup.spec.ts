b2633f0cbb29db2cf8ad07ab57dc982bdd5c2301
Jonathan Galdino <johnzballad@gmail.com>
2022-02-05 23:54:49 -0300

    refactor: change mocking strategy

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -18,17 +18,7 @@ const makeEmailValidator = (): EmailValidator => {
 
   return emailValidatorStub
 }
-const makeEmailValidatorWithError = (): EmailValidator => {
-  class EmailValidatorStub implements EmailValidator {
-    isValid (email: string): boolean {
-      throw new Error()
-    }
-  }
 
-  const emailValidatorStub = new EmailValidatorStub()
-
-  return emailValidatorStub
-}
 const makeSut = (): SutTypes => {
   const emailValidatorStub = makeEmailValidator()
   const sut = new SignUpController(emailValidatorStub)
@@ -141,8 +131,10 @@ describe('SignUp Controller', () => {
   })
 
   it('should return 500 if an EmailValidator throws', () => {
-    const emailValidatorStub = makeEmailValidatorWithError()
-    const sut = new SignUpController(emailValidatorStub)
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error()
+    })
 
     const httpRequest = {
       body: {
